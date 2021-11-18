fcdcce4ef134181328840468fe32bf16b98ac52b
CJBiohacker <cj.moyses@gmail.com>
2021-11-17 21:25:41 -0300

    "Clean code Switch para Array"

--- snippets-de-arrays.js
+++ snippets-de-arrays.js
@@ -18,3 +18,32 @@ const MediaDoVetor = (vetor) => {
 
 console.log(MediaDoVetor([1, 2, 3, 4, 5, 6]));
 /* -------------------------------------------------------------------------------------------------- */
+// Clean code de uma condicional switch..case para Array.
+
+function escolherItem(id) {
+    switch (id) {
+        case 0:
+            return 'Paul Atreides';
+        case 1:
+            return 'Lady Jessica';
+        case 2:
+            return 'Barão Vladimir Harkonnen';
+        case 3:
+            return 'Chani';
+        case 4:
+            return 'Duncan Idaho';
+        case 5:
+            return 'Stilgar';
+        default:
+            return 'Nº Inválido';
+    }
+}
+
+const escolherItemId = (idNumber) => {
+    const duneChars = ['Paul Atreides', 'Lady Jessica', 'Barão Vladimir Harkonnen', 'Chani', 'Duncan Idaho', 'Stilgar'];
+
+    return duneChars[idNumber]
+}
+
+console.log(escolherItemId(5));
+console.log(escolherItem(2));
