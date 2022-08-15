d54d853721d198069db34fe14aebe39e11b67758
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2022-08-15 18:25:34 -0300

    feat : using spread operator to manipulate arrays

--- snippets-de-arrays.js
+++ snippets-de-arrays.js
@@ -1,19 +1,20 @@
 /* -------------------------------------------------------------------------------------------------- */
 // Função pra calcular média que se adapta de acordo com o nº de itens inseridos.
 const MediaDoVetor = (vetor) => {
-    let soma = 0;
+  let soma = 0;
 
-    vetor.forEach(elemento => {                      // Utilizando forEach
-        soma = soma + elemento
-    });
+  vetor.forEach((elemento) => {
+    // Utilizando forEach
+    soma = soma + elemento;
+  });
 
-    // for (let i = 0; i < vetor.length; i++) {     // Utilizando for
-    //     soma = soma + vetor[i];
-    // };
+  // for (let i = 0; i < vetor.length; i++) {     // Utilizando for
+  //     soma = soma + vetor[i];
+  // };
 
-    let media = soma / vetor.length;
+  let media = soma / vetor.length;
 
-    return media;
+  return media;
 };
 
 console.log(MediaDoVetor([1, 2, 3, 4, 5, 6]));
@@ -21,29 +22,42 @@ console.log(MediaDoVetor([1, 2, 3, 4, 5, 6]));
 // Clean code de uma condicional switch..case para Array.
 
 function escolherItem(id) {
-    switch (id) {
-        case 0:
-            return 'Paul Atreides';
-        case 1:
-            return 'Lady Jessica';
-        case 2:
-            return 'Barão Vladimir Harkonnen';
-        case 3:
-            return 'Chani';
-        case 4:
-            return 'Duncan Idaho';
-        case 5:
-            return 'Stilgar';
-        default:
-            return 'Nº Inválido';
-    }
+  switch (id) {
+    case 0:
+      return "Paul Atreides";
+    case 1:
+      return "Lady Jessica";
+    case 2:
+      return "Barão Vladimir Harkonnen";
+    case 3:
+      return "Chani";
+    case 4:
+      return "Duncan Idaho";
+    case 5:
+      return "Stilgar";
+    default:
+      return "Nº Inválido";
+  }
 }
 
 const escolherItemId = (idNumber) => {
-    const duneChars = ['Paul Atreides', 'Lady Jessica', 'Barão Vladimir Harkonnen', 'Chani', 'Duncan Idaho', 'Stilgar'];
+  const duneChars = [
+    "Paul Atreides",
+    "Lady Jessica",
+    "Barão Vladimir Harkonnen",
+    "Chani",
+    "Duncan Idaho",
+    "Stilgar",
+  ];
 
-    return duneChars[idNumber]
-}
+  return duneChars[idNumber];
+};
 
 console.log(escolherItemId(5));
 console.log(escolherItem(2));
+/* -------------------------------------------------------------------------------------------------- */
+// Manipulação de arrays com o Spread Operator.
+const items = [1, 2, 3, 4, 5];
+const [ , , ...resto] = items;  // Desestruturação da array principal utilizando Spread Operator para coletar o resto de uma array, baseado no índice do campo de desestruturação
+console.log(resto);
+
