6ca32c920ad38df0eaef79ef3190be2f7a99566a
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2023-01-31 15:14:08 -0300

    feat: moment js date conversion and experiments

---
+++ JavaScript/quick-notes.js
@@ -0,0 +1,26 @@
+const _ = require('lodash');
+const moment = require('moment');
+
+const users = [
+    { user: 'barney', color:'black' },
+    { user: 'fred', color:'green' },
+    { user: 'honey', color:'blue'},
+    { user: 'annete', color:'yellow'},
+    { user: 'zennya', color:'white' },
+  ];
+
+const iterate = (x) => {
+    return x === 'barney'
+}
+
+  const mappingUser = _.map(users, 'user');
+  const iteratingUsers = _.map(mappingUser, iterate)
+  console.log("🚀 ~ file: quick-notes.js:10 ~ mappingUser", mappingUser)
+  console.log("🚀 ~ file: quick-notes.js:10 ~ iteratingUsers", iteratingUsers)
+
+const currentUsers = users.map( x => x.date ='2022-10-21T14:39:44.338Z');
+console.log("🚀 ~ file: quick-notes.js:21 ~ users ==> ", users)
+
+users[0].date = moment(String(users[0].date)).utc().format("YYYY-MM-DD HH:mm Z");
+console.log("🚀 ~ file: quick-notes.js:21 ~ users ==> ", users)
+
