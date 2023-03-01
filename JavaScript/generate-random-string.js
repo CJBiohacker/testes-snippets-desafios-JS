6af7e1a37ccbe70a8de1f34c5e2a64c343b69201
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2023-03-01 16:11:33 -0300

    add more description

--- JavaScript/generate-random-string.js
+++ JavaScript/generate-random-string.js
@@ -1,4 +1,5 @@
 // Random String Generator (can be used as a password/security key generator)
+// Change the toString(radix) to change the numeric system (binary, octal, hexadecimal, hexatridecimal, etc)
 const randomString = Math.random().toString(36).slice(2);
 
 console.log(randomString);
