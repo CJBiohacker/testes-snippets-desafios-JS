c2aa73a3f5c85a519c26ffc0a93482cdc1547250
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2023-03-13 18:57:19 -0300

    feat: add random int number function generator

--- JavaScript/math-algoritms.js
+++ JavaScript/math-algoritms.js
@@ -13,3 +13,10 @@ export const findGCD = (a, b) => {
     return findGCD(b, a % b);
   }
 };
+
+// Getting a random integer between two values including the max value.
+export const getRandomIntNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
