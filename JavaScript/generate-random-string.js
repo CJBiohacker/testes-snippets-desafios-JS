966678ddb725ede66e6f3e663baf391f82ffe27e
Carlos de Lima Junior <carlos.junior@zooxsmart.com>
2023-03-24 18:47:12 -0300

    ✨ feat: improve generate random string adding a special char as last element

--- JavaScript/generate-random-string.js
+++ JavaScript/generate-random-string.js
@@ -3,3 +3,38 @@
 const randomString = Math.random().toString(36).slice(2);
 
 console.log(randomString);
+
+const regexRules = {
+  lengthRule: /^.{8,}$/,
+  upperCaseRule: /(?=.*[A-Z])/,
+  lowerCaseRule: /(?=.*[a-z])/,
+  numericRule: /(?=.*[0-9])/,
+  specialCharRule: /(.*\W.*)/,
+};
+
+const specialChars = "!@#$%*()-_+=`[]{}^~,.;/<>ºª|'*+.";
+const generateRandomSpecialChar = () => {
+  return specialChars[Math.floor(Math.random() * specialChars.length)];
+};
+
+const findFirstLowerCaseAndCapitalizeIt = (randomString) => {
+  const stringToArray = Array.from(randomString);
+  const findLowerCase = stringToArray.find((x) =>
+    regexRules.lowerCaseRule.test(x)
+  );
+  const specialChar = generateRandomSpecialChar();
+  console.log("special Char ==> ", specialChar);
+  stringToArray.push(specialChar);
+  const lowerToUpper = findLowerCase.toLocaleUpperCase();
+
+  return stringToArray
+    .map((x) => x.replace(findLowerCase, lowerToUpper))
+    .join("")
+    .toLocaleString();
+};
+
+const randomStrongPwd = findFirstLowerCaseAndCapitalizeIt(randomString);
+
+console.log(randomStrongPwd);
+
+export default { findFirstLowerCaseAndCapitalizeIt };
